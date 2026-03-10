3ab7976531a29a3e61235c09f7d1e54609ea1fb0
fathullation <fathullation@gmail.com>
2026-03-11 06:13:46 +0800

    Fix path with explicit extension

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@ import { defineConfig } from 'astro/config';
 import sanity from "@sanity/astro";
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
-    integrations: [
-      tailwind({
-      configFile: './tailwind.config.mjs',
-      applyBaseStyles: false,
-    }),
-    sanity({
+    integrations: [tailwind({
+    configFile: './tailwind.config.mjs',
+    applyBaseStyles: false,
+  }), sanity({
       projectId: process.env.PUBLIC_SANITY_PROJECT_ID || "5m3eg18d", 
       dataset: process.env.PUBLIC_SANITY_DATASET || "production",
         useCdn: false, 
         apiVersion: "2024-03-10",
-        }),
-    ],
+        }), react()],
 });
